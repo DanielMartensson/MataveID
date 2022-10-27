3afa1d311d8a559d2c638e939fada48de13ff8a1
Daniel Mårtensson <daniel.martensson100@outlook.com>
2022-10-27 23:38:46 +0200

    Update SPA - Better

--- sourcecode/spa.m
+++ sourcecode/spa.m
@@ -3,44 +3,52 @@
 % Output: amp(amplitude), wout(frequencies)
 % Example 1: [amp, wout] = spa(y, t);
 % Author: Daniel Mårtensson, November 2017
+% Update 2022-10-27 with better frequencies and amplitudes
 
 function [amp, wout] = spa(varargin)
   % Check if there is any input
   if(isempty(varargin))
-    error('Missing imputs')
+    error('Missing inputs')
   end
 
-  
   % Get output
   if(length(varargin) >= 1)
     y = varargin{1};
   else
     error('Missing output');
   end
-  
+
   % Get time
   if(length(varargin) >= 2)
     t = varargin{2};
   else
     error('Missing time');
   end
-  
-  Ts = t(2)-t(1); % Sampling time
-  Fs = 1/Ts; % Sampling rate
+
+  % Get sampling rate
+  sampleTime = t(2)-t(1);
+  Fs = 1/sampleTime;
+
   % Do FFT
-  F = fft(y, Fs);
+  F = fft(y);
   Y = abs(F);
-  
-  % Compute the frequencies 
-  freq = (0:Fs-1)(1:end/2 +1);
+
+  % Compute the frequencies
+  n = length(y);
+  freq = (0:n-1)*(Fs/n);
+
   % Compute the amplitudes
-  Y = Y(1:end/2)/length(Y)*2;
-  
+  Y = abs(Y)/n*2;
+
+  % Cut away the mirror
+  Y = Y(1:end/2);
+  freq = freq(1:end/2);
+
   plot(freq, Y)
   ylabel('Amplitude')
   xlabel('Frequency [Hz]')
   grid on
-  
+
   % Return values
   amp = 20*log10(Y);
   wout = freq;
