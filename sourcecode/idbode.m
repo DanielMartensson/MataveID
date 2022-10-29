f6fa1d7ab047873852371ba8f583b6e1d6a1d5f1
Daniel Mårtensson <daniel.martensson100@outlook.com>
2022-10-29 17:01:23 +0200

    Update idbode.m

--- sourcecode/idbode.m
+++ sourcecode/idbode.m
@@ -1,67 +1,92 @@
 % Plot a bode diagram from frequency data
-% Input: u(frequency input), y(frequency response), w(frequencies)
-% Example: idbode(u, y, w)
+% Input: u(frequency input), y(frequency response), fs(sampleTime)
+% Example: idbode(u, y, fs)
 % Author: Daniel Mårtensson, April 2020
+% Update 2022, Oktober 29:e Better plot
 
 function idbode(varargin)
   % Check if there is any input
   if(isempty(varargin))
-    error('Missing imputs')
+    error('Missing inputs')
   end
-  
+
   % Get input
   if(length(varargin) >= 1)
     u = varargin{1};
   else
     error('Missing input')
   end
-  
+
   % Get output
   if(length(varargin) >= 2)
     y = varargin{2};
   else
     error('Missing output')
   end
-  
-  % Get the frequencies
+
+  % Get the sampleTime
   if(length(varargin) >= 3)
-    w = varargin{3};
+    sampleTime = varargin{3};
   else
-    error('Missing frequencies');
+    error('Missing time');
   end
-  
+
   % Get the size of u or y and w
-  m = size(y, 1);
-  n = size(w, 2);
-  l = n/2;
-  
+  [m, n] = size(y);
+
   % Do Fast Fourier Transform for every input signal
   for i = 1:m
+
     % Do FFT
-    fy = fft(y(i, 1:n));
-    fu = fft(u(i, 1:n));
-    
-    % Cut the frequency into half too and multiply it with 4
-    W = w(i, 1:l)*4; 
-    if(W(i, 1) <= 0)
-      W(1) = W(2); % Prevent zeros on the first index. In case if you used w = linspace(0,...
-    end
-    
-    % Create the complex magnitudes and cut it to half
-    H = (fy./fu)(1:l); 
-    
+    Nfft = 32768; % 2^15 is a good number to get a clean plot
+    fy = fft(y(i, :),Nfft);
+    fu = fft(u(i, :) ,Nfft);
+    H = fy./fu;
+
+    % Frequencies
+    freq = [0:Nfft-1]/Nfft*2*pi*sampleTime;
+
+    % Avoid zero
+    freq(1) = freq(2);
+
+    % Windowing - Half
+    H = H(1:end/2);
+    freq = freq(1:end/2);
+
     % Plot the bode diagram of measurement data
-    figure('Name', sprintf(strcat('Transfer function: ', num2str(i))))
+    figure('Name', sprintf(strcat('Bode diagram: ', num2str(i))))
     subplot(2,1,1)
-    semilogx(W(i, 1:l), 20*log10(abs(H)));
+    semilogx(freq, 20*log10(abs(H)));
     ylabel('Magnitude [dB]');
     grid on
     subplot(2,1,2)
-        
-    BodeAngles = angle(H) * 180/pi;
-    semilogx(W(i, 1:l), BodeAngles);
+
+    % Create the bode angles
+    BodeAngles = zeros(1, length(H));
+
+    % This fixing the 360 degrees problem
+    R = 0;
+    for k = 1:length(H)-1
+      % Get the angle in degrees
+      a1 = angle(H(k)) * 180/pi;
+      a2 = angle(H(k+1)) * 180/pi;
+
+      % Add angle
+      BodeAngles(k) = a1 + R;
+
+      % Check if we are aboute 90
+      if(and(a2 > 170, a1 < -170))
+        R = R - 360
+      elseif(and(a1 > 170, a2 < -170))
+        R = R + 360
+      end
+    end
+    BodeAngles(end) = angle(H(end)) * 180/pi + R;
+
+    % Plot
+    semilogx(freq, BodeAngles);
     ylabel('Phase [deg]');
     xlabel('Frequency [rad/s]');
-    grid on 
+    grid on
   end
 end
