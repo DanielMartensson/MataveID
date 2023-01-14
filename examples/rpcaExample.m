815ee79276197354fffea4a33de74affa447743b
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-01-14 19:35:44 +0100

    Update rpcaExample.m

--- examples/rpcaExample.m
+++ examples/rpcaExample.m
@@ -1,6 +1,6 @@
 clc; clear; close all;
 
-X = imread('bob.jpg'); % Load Mr Bob
+X = imread('..\pictures\bob.png'); % Load Mr Bob
 X = rgb2gray(X);       % Grayscale 8 bit
 X = double(X);         % Must be double 40 => 40.0
 [L, S] = rpca(X);      % Start RPCA. Our goal is to get L matrix
