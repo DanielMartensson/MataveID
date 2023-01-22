f745ed82390da169ab3d36bc3e015c8c01e0b7b9
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-01-23 00:26:51 +0100

    Update armax.m

--- sourcecode/armax.m
+++ sourcecode/armax.m
@@ -122,6 +122,7 @@ function [sysd, K] = armax(varargin)
 
     % Compute Q, R, S for the riccati equation
     nx = size(Ad, 2);
+    l = size(y, 1);
     Q = covariance(1:nx, 1:nx);
     R = covariance(nx+1:nx+l, nx+1:nx+l);
     S = covariance(1:nx, nx+1:nx+l);
