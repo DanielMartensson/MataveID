e738f13b43c1b0225e178fca7d11aded0dcb4446
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-02-07 00:32:18 +0100

    SVM.m update - Easier to use

--- sourcecode/svm.m
+++ sourcecode/svm.m
@@ -3,44 +3,61 @@
 % Output: X_point(coordinates in x-axis), Y_point(coordinates in y-axis), amount_of_supports_for_class(how many points for each class)
 % Example 1: [X_point, Y_point, amount_of_supports_for_class] = svm(X, Y);
 % Author: Daniel Mårtensson, December 2021
+% Update 2023-02-07: Now some points can be avoided, easier to create boundaries
 
 function [X_point, Y_point, amount_of_supports_for_class] = svm(X, Y)
-  % First print our data 
-  [legend_labels] = print_data(X, Y);
-  
-  % Then place out our support points 
-  [X_point, Y_point, amount_of_supports_for_class] = place_out_supports_points(X, Y, legend_labels);
-  
+  % Then place out our support points
+  [X_point, Y_point, amount_of_supports_for_class] = place_out_supports_points(X, Y);
+
+  % Print data
+  print_data(X, Y, X_point, Y_point, amount_of_supports_for_class);
+
   % Check if our support points works
   check_points_support(X, Y, X_point, Y_point);
-  
-  % Ask the user what the file name should have 
+
+  % Ask the user what the file name should have
   [file_name] = ask_user_about_file_name();
-  
+
   % Generate C code
   [c_source, c_header, X_point, Y_point, amount_of_supports_for_class] = generate_c_code(X_point, Y_point, amount_of_supports_for_class, file_name);
-  
-  % Save the C code into a file 
+
+  % Save the C code into a file
   save_c_code_into_a_file(c_source, c_header, file_name);
 end
 
-function [legend_labels] = print_data(X, Y)
-  % Get the amount of classes 
+function print_data(X, Y, X_point, Y_point, amount_of_supports_for_class)
+  % Get the amount of classes
   c = size(X, 1);
-    
-  % Plot the data classes 
+
+  % Clear figure and add grid
+  clf
+  grid on
+  title('Data and boundaries', 'FontSize', 30);
+
+  % Create labels holder
   legend_labels = {};
+
+  % Print out the data
   for i = 1:c
-    scatter(X(i, :), Y(i, :));
     hold on
+    scatter(X(i, :), Y(i, :));
     legend_labels{i} = sprintf('Data %i', i);
   end
-  legend(legend_labels);
-  grid on
+
+  % Create labels
+  legend(legend_labels, 'AutoUpdate', 'off');
+
+  % Now print the boundaries
+  for i = 1:c
+    hold on
+    L = amount_of_supports_for_class(i);
+    plot(X_point(i, 1:L), Y_point(i, 1:L))
+  end
+
 end
 
-function [X_point, Y_point, amount_of_supports_for_class] = place_out_supports_points(X, Y, legend_labels)
-  % Get the amount of classes 
+function [X_point, Y_point, amount_of_supports_for_class] = place_out_supports_points(X, Y)
+  % Get the amount of classes
   c = size(X, 1);
 
   % Start set out the supports points
@@ -48,6 +65,11 @@ function [X_point, Y_point, amount_of_supports_for_class] = place_out_supports_p
   Y_point = zeros(c, 256);
   amount_of_supports_for_class = zeros(1, c);
   for i = 1:c
+    % Clear figure
+    clf
+    scatter(X(i, :), Y(i, :), 'b');
+    grid on
+
     % buttonnumber == 1 indicates left click
     % buttonnumber == 3 indicates right click
     buttonnumber = 1;
@@ -56,69 +78,65 @@ function [X_point, Y_point, amount_of_supports_for_class] = place_out_supports_p
     xlabel('Left click to set out a point. Right click to go to next data class');
     while buttonnumber == 1
       % Left-click on the scatter plot
-      [x, y, buttonnumber] = ginput(1); 
-      
+      [x, y, buttonnumber] = ginput(1);
+
       % Abort to next data set
       if(buttonnumber == 3)
         break;
       end
-      
-      % Place the x and y - 
+
+      % Place the x and y -
       X_point(i, j) = x;
       Y_point(i, j) = y;
-      
+
       % Remember how many points we have set ut for each data class
       amount_of_supports_for_class(i) = amount_of_supports_for_class(i) + 1;
-      
-      % Plot the dots as a line 
+
+      % Plot the dots as a line
       X_line = X_point(i, 1:j);
       Y_line = Y_point(i, 1:j);
+      hold on
       if(length(X_line) == 1)
-        scatter(X_line, Y_line, '.')  
+        scatter(X_line, Y_line, '.')
       else
-        plot(X_line, Y_line, '-k')  
+        plot(X_line, Y_line, '-k')
       end
-      
-      % Place out the legends so we don't get new legends
-      legend(legend_labels)
-      
-      % Increment 
+
+      % Increment
       j = j + 1;
     end
-    
+
     % The last line will close the gap between the end and the beginning
     X_point(i, j) = X_point(i, 1);
     Y_point(i, j) = Y_point(i, 1);
-      
+
     % Remember how many points we have set ut for each data class
     amount_of_supports_for_class(i) = amount_of_supports_for_class(i) + 1;
-      
-    % Plot the points as a line 
+
+    % Plot the points as a line
     X_line = X_point(i, 1:j);
     Y_line = Y_point(i, 1:j);
-    plot(X_line, Y_line, '-k')  
+    plot(X_line, Y_line, '-k')
 
-    % Place out the legends so we don't get new legends
-    legend(legend_labels)
   end
 end
 
 function check_points_support(X, Y, X_point, Y_point)
-  % Get the amount of classes 
+  % Get the amount of classes
   c = size(X_point, 1);
 
   % Do a quick check if the SVM points are correctly placed
   for i = 1:c
-    % Get the points 
+    % Get the points
     all_X_points = X_point(i, :);
     all_Y_points = Y_point(i, :);
-    
-    % Begin to count 
+
+    % Begin to count
     counted_points = sum(inpolygon(X(i,:) , Y(i, :), all_X_points, all_Y_points));
     if(counted_points == length(X(i, :)))
-      disp(sprintf('Checking for class %i is OK - Good generated SVM supports', i));
+      disp(sprintf('Checking for class %i - All points include in the boundary', i));
     else
-      error(sprintf('Checking for class %i failed beacause the generated SVM points excluding data', i));
+      disp(sprintf('Checking for class %i - Not all points are included in the boundary', i));
     end
   end
 end
@@ -130,29 +148,29 @@ function [file_name] = ask_user_about_file_name()
   end
 end
 
-function [c_source, c_header, X_point, Y_point, amount_of_supports_for_class] = generate_c_code(X_point, Y_point, amount_of_supports_for_class, file_name)  
-  % Find maximum column for X_point_str and Y_point_str 
+function [c_source, c_header, X_point, Y_point, amount_of_supports_for_class] = generate_c_code(X_point, Y_point, amount_of_supports_for_class, file_name)
+  % Find maximum column for X_point_str and Y_point_str
   len_px_py = max(amount_of_supports_for_class);
-  
+
   % Create svm_classes
   svm_classes = size(X_point, 1);
-  
+
   % Cut the matrix so we can avoid all zeros
   X_point = X_point(:, 1:len_px_py);
   Y_point = Y_point(:, 1:len_px_py);
-  
+
   % Create px matrix with 4 decimals - Notice that X_point must be transpose
   px = sprintf('%0.4ff,', X_point');
   px(end) = ''; % Remove the last ','
-  
+
   % Create py matrix with 4 decimals - Notice that Y_point must be transpose
   py = sprintf('%0.4ff,', Y_point');
   py(end) = ''; % Remove the last ','
-  
+
   % Create p array
   p = sprintf('%i,', amount_of_supports_for_class);
   p(end) = ''; % Remove the last ','
-  
+
   % Write this c source into a file
   c_source = {sprintf('#include "%s.h"', file_name);
   '/*'
@@ -182,7 +200,7 @@ function [c_source, c_header, X_point, Y_point, amount_of_supports_for_class] = 
   '    for(uint16_t j = 0; j < m; j++)'
   '      point_counter_list[i] += inpolygon(x[j], y[j], &px[i*len_px_py], &py[i*len_px_py], p[i]);'
   '}'};
-  
+
   % Create the header file
   c_header = {sprintf('#ifndef %s', strcat(upper(file_name), '_H_'));
   sprintf('#define %s', strcat(upper(file_name), '_H_'))
@@ -200,25 +218,25 @@ function [c_source, c_header, X_point, Y_point, amount_of_supports_for_class] = 
   '#endif'
   ''
   '#endif'};
-  
+
 end
 
 function save_c_code_into_a_file(c_source, c_header, file_name)
-  % Create the source file 
+  % Create the source file
   fid = fopen(strcat(file_name, '.c'), 'wt');
   for i = 1:size(c_source, 1)
     fprintf(fid, "%s\n", cell2mat(c_source(i)));
   end
   fclose(fid);
-  
-  % Create the header file 
+
+  % Create the header file
   fid = fopen(strcat(file_name, '.h'), 'wt');
   for i = 1:size(c_header, 1)
     fprintf(fid, "%s\n", cell2mat(c_header(i)));
   end
   fclose(fid);
-  
+
   % Tell the users about the fantastic news
   disp(sprintf('The source file %s.c and header file %s.h has been generated at the location %s', file_name, file_name, pwd));
-  
+
 end
