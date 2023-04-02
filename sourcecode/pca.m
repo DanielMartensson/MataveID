bc097f756f7b14455d181842a1a760ca56843da9
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-04-02 20:25:30 +0200

    Add files via upload

---
+++ sourcecode/pca.m
@@ -0,0 +1,39 @@
+% Principal Component Analysis
+% Input: X(Data), c(Amount of components)
+% Output: Projected matrix P, Project matrix W
+% Example 1: [P, W] = pca(X, c);
+% Author: Daniel Mårtensson, 2023 April
+
+function [P, W] = pca(varargin)
+  % Check if there is any input
+  if(isempty(varargin))
+    error('Missing inputs')
+  end
+
+  % Get impulse response
+  if(length(varargin) >= 1)
+    X = varargin{1};
+  else
+    error('Missing data X')
+  end
+
+  % Get the amount of components
+  if(length(varargin) >= 2)
+    c = varargin{2};
+  else
+    error('Missing amount of components');
+  end
+
+  % Average
+  mu = mean(X);
+
+  % Center data
+	Y = X - mu;
+
+  % PCA analysis
+	[U, S, V] = svd(Y, 0);
+
+  % Projection
+	W = V(:, 1:c);
+	P = X*W;
+end
