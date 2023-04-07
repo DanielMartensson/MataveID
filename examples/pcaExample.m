2ac5c33d5e100cffa2dcd5f4212b6006841e64a5
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-04-07 16:12:10 +0200

    Update pcaExample.m

--- examples/pcaExample.m
+++ examples/pcaExample.m
@@ -1,3 +1,8 @@
+% First clear and close all figures
+clear
+close all
+
+% Create data where every colum is a variable
 X = [1 3 10;
     2 4 12;
     3 6 13;
@@ -10,9 +15,28 @@ X = [1 3 10;
     34 89 131;
     35 103 139];
 
-[P, W] = pca(X, 2);
-
-figure(1)
+% Plot original data
 scatter3(X(:, 1), X(:, 2), X(:, 3))
-figure(2)
-scatter(P(:, 2), P(:, 1));
+title('Original 3D data', 'FontSize', 20)
+
+% Reduce how many dimensions
+c = 2;
+
+% Do PCA for 2D
+[P, W] = pca(X, c);
+
+% Show 2D reduction
+figure
+scatter(P(:, 1), P(:, 2));
+grid
+title('Dimension reduction to 2D', 'FontSize', 20)
+
+% Reduce how many dimensions
+c = 1;
+
+% Do PCA for 1D
+[P, W] = pca(X, c);
+figure
+scatter(P(:, 1), 0*P(:, 1));
+grid
+title('Dimension reduction to 1D', 'FontSize', 20)
