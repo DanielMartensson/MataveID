ea3531a1df770b3ecb685d602d7482d118a74515
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-05 13:46:30 +0200

    Faster PCA with eig instead of SVD

--- matave/+mi/pca.m
+++ matave/+mi/pca.m
@@ -30,13 +30,61 @@ function [P, W, mu] = pca(varargin)
 
   % Center data
   Y = X - mu;
-  
+
   % PCA analysis
-  [U, ~, ~] = svd(Y, 0);
+  abort = input(sprintf('The size of the matrix is %ix%i. Do you want to apply PCA onto it? 1 = Yes, 0 = No: ', size(Y)));
+  if(abort == 1)
+    [U, ~, ~] = svdecon(Y);
+  else
+    error('Aborted');
+  end
 
   % Projection
   W = U(:, 1:c);
   P = W'*Y;
+end
+
+function [U,S,V] = svdecon(X)
+  % Input:
+  % X : m x n matrix
+  %
+  % Output:
+  % X = U*S*V'
+  %
+  % Description:
+  % Does equivalent to svd(X,'econ') but faster
+  %
+  % Vipin Vijayan (2014)
+  %X = bsxfun(@minus,X,mean(X,2));
+  [m,n] = size(X);
+  if  m <= n
+      C = X*X';
+      [U,D] = eig(C);
+      clear C;
+
+      [d,ix] = sort(abs(diag(D)),'descend');
+      U = U(:,ix);
+
+      if nargout > 2
+          V = X'*U;
+          s = sqrt(d);
+          V = bsxfun(@(x,c)x./c, V, s');
+          S = diag(s);
+      end
+  else
+      C = X'*X;
+      [V,D] = eig(C);
+      clear C;
+
+      [d,ix] = sort(abs(diag(D)),'descend');
+      V = V(:,ix);
+
+      U = X*V; % convert evecs from X'*X to X*X'. the evals are the same.
+      %s = sqrt(sum(U.^2,1))';
+      s = sqrt(d);
+      U = bsxfun(@(x,c)x./c, U, s');
+      S = diag(s);
+  end
 end
 
 function X = cluster_filter(X)
