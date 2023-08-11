a7876d7a239111281baff847bdbd70c7fb7dda5c
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-11 08:43:23 +0200

    Update lda.m

--- matave/+mi/lda.m
+++ matave/+mi/lda.m
@@ -91,7 +91,7 @@ function [P,W] = lda(varargin)
   [V, D] = eig(Sb, Sw);
 
   % Compute the rank
-  fprintf('The maximal rank of the dimension reduction is: %i\n', rank(Sw));
+  fprintf('The maximal rank of the LDA dimension reduction is: %i\n', rank(Sw));
 
   % Sort eigenvectors descending by eigenvalue
   [D, idx] = sort(diag(D), 1, 'descend');
