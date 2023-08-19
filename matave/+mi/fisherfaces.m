8472c7b40a076c6976427fae5cda697b8a697d0c
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-19 16:29:41 +0200

    Update fisherfaces.m

--- matave/+mi/fisherfaces.m
+++ matave/+mi/fisherfaces.m
@@ -108,7 +108,7 @@ function fisherfaces_train_neural_network_model()
   model_b = bias;
 
   % Check accuracy
-  X = weight*images' + bias;
+  X = weight*images + bias;
   classes = length(class_id);
   score = 0;
   for i = 1:classes
