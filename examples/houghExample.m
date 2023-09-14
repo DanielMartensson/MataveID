a8ff31b242db6f1cd2c56c70be7bcc1d0c847329
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-09-14 09:31:26 +0200

    Update houghExample.m

--- examples/houghExample.m
+++ examples/houghExample.m
@@ -15,9 +15,10 @@ end
 imshow(X);
 
 % Do hough transform
-N = 6; % Number of lines
-radius = 10; % This tuning parameter prevents the algorithm to re-select the same line again
-[K, M] = mi.hough(X, N, radius);
+p = 0.3; % Percentage definition of a line e.g all lines shorter than p times longest line, should be classes as a non-line
+epsilon = 10; % Minimum radius for hough cluster
+min_pts = 2; % Minimum points for hough cluster
+[N, K, M] = mi.hough(X, p, epsilon, min_pts);
 
 % Plot the lines together with the image
 [~, n] = size(X);
