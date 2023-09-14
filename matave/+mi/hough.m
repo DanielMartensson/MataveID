6852df845e62f6a46dd99c732e086759e5fd21f7
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-09-14 09:29:55 +0200

    Robust Hough Transform - line detection

--- matave/+mi/hough.m
+++ matave/+mi/hough.m
@@ -1,11 +1,10 @@
-% Hough transform - Line detection algorthm
-% Input: X(Data matrix of an edge image), N(Amout of sloped lines), p(Line length threshold in precent)
-% Output: K(Slopes), M(Bias), R(Projected distance), T(Radians angle of the projected distance)
-% Example 1: [K, M, R, T] = mi.hough(X, N);
-% Example 2: [K, M, R, T] = mi.hough(X, N, p);
-% Author: Daniel Mårtensson, 10 September 2023
+% Hough transform - Line detection algorthm by using hough transform and DBscan
+% Input: X(Data matrix of an edge image), p(Line length threshold in precent), epsilon(Minimum radius for hough cluster), min_pts(Minimum points for hough cluster)
+% Output: N(Number of lines), K(Slopes), M(Bias), R(Projected distance), T(Radians angle of the projected distance)
+% Example 1: [N, K, M, R, T] = mi.hough(X, p, epsilon, min_pts);
+% Author: Daniel Mårtensson, 14 September 2023
 
-function [K, M, R, T] = hough(varargin)
+function [N, K, M, R, T] = hough(varargin)
   % Check if there is any input
   if(isempty(varargin))
     error('Missing inputs')
@@ -17,40 +16,66 @@ function [K, M, R, T] = hough(varargin)
   else
     error('Missing data X')
   end
-
   % Get amount of sloped lines
   if(length(varargin) >= 2)
-    N = varargin{2};
-  else
-    error('Missing amount of sloped lines')
-  end
-
-  % Get amount of sloped lines
-  if(length(varargin) >= 3)
-    p = varargin{3};
+    p = varargin{2};
+    if(p <= 0)
+      error('Line length threshold in precent needs to be above 0');
+    end
     if(p > 1)
       p = 1;
     end
   else
-    p = 0;
+    error('Missing line length threshold in precent');
+  end
+
+  % Get minimum radius for hough cluster
+  if(length(varargin) >= 3)
+    epsilon = varargin{3};
+  else
+    error('Missing minimum radius for hough cluster');
+  end
+
+  % Get minimum points for hough cluster
+  if(length(varargin) >= 4)
+    min_pts = varargin{4};
+  else
+    error('Missing minimum points for hough cluster');
   end
 
   % Compute scores for the lines
-  P = compute_scores(X, p);
+  P = hough_scores(X, p);
 
-  % Turn P into vectors
-  [x, y, z] = matrix_into_vectors(P);
+  % Turn the scores matrix into a hough cluster
+  [A, N, index] = hough_cluster(P, epsilon, min_pts);
 
   % Compute lines
-  radius = 10;
-  [K, M, R, T] = compute_lines(P, N, radius);
+  [K, M, R, T] = hough_lines(A, N, index);
 end
 
-function [x, y, z] = matrix_into_vectors(P)
+function [A, N, index] = hough_cluster(P, epsilon, min_pts)
+  % Turn matrix P into 2 vectors
   [x, y, z] = find(P);
+
+  % Do dbscan - All idx values that are 0, are noise
+  A = [x y];
+  index = mi.dbscan(A, epsilon, min_pts);
+
+  % Add the z axis as well
+  A = [x y z];
+
+  % find the amount of clusters
+  N = max(index);
+
+  % Uncomment for test
+  %for i = 0:N
+  %  scatter3(A(index == i, 1), A(index == i, 2), A(index == i, 3));
+  %  hold on
+  %end
+  %figure
 end
 
-function P = compute_scores(X, p)
+function P = hough_scores(X, p)
   % Get the size of X
   [m, n] = size(X);
 
@@ -67,7 +92,7 @@ function P = compute_scores(X, p)
   r_max = floor(sqrt(m^2 + n^2));
 
   % Create points holder P
-  P = zeros(270, r_max);
+  P = zeros(180, r_max);
 
   % Collect the points for the most common lines
   for i = 1:m
@@ -90,134 +115,60 @@ function P = compute_scores(X, p)
       % Compute r and make it to an integer
       r = floor(sqrt(x.^2 + y.^2)) + 1; % + 1 is just for indexing
 
-      % Compute theta and make sure theta is not negative
-      v = rad2deg(atan2(y, x));
-      theta = floor(90 + v);
+      % Compute the angles
+      angles = atan2(y, x);
 
-      % If theta is deliberately, purposefully and exactly 180
+      % Make sure that the angles are not negative
+      angles(angles < 0) = angles(angles < 0) + pi;
+
+      % Turn them into degrees
+      angles = floor(rad2deg(angles)) + 1;
+
+      % If angles is deliberately, purposefully and exactly 90
       % then the slope is going to be infinity large
-      % Make sure it's either 179 or 181 by random choice
-      theta(theta == 180) = 180 + sign(rand(1));
+      % Make sure theta is either 179 or 181 by random choice
+      angles(angles == 90) = 90 + sign(rand(1));
 
       % Avoid values that are larger than r_max
-      theta(r > r_max) = [];
+      angles(r > r_max) = [];
       r(r > r_max) = [];
 
       % Add + 1
-      indices = sub2ind(size(P), theta, r);
+      indices = sub2ind(size(P), angles, r);
       P(indices) = P(indices) + 1;
     end
   end
-
-  % This is the non vectorized code
-  %for i = 1:m
-  %  for j = 1:n
-      % Check if the coordinate X(i, j) belongs to an edge
-  %    if(X(i, j) <= eps)
-       % No edge here
-  %      continue
-  %    end
-
-      % Compute the r and theta from the pixel
-  %    for k = 1:K_length
-        % Compute M from straight line equation
-  %     M = j - K(k)*i;
-
-        % Find x that finds a minimal r
-  %     x = -Kk(k)*Mk/(1 + Kk(k)^2);
-
-        % Compute y
-  %     y = K(k)*x + M;
-
-        % Compute r and make it to an integer
-  %     r = floor(sqrt(x^2 + y^2)) + 1; % + 1 is just for indexing
-
-        % Compute theta and make sure theta is not negative
-  %     v = rad2deg(atan2(y, x));
-  %     theta = floor(90 + v);
-
-        % If theta is deliberately, purposefully and exactly 180
-        % then the slope is going to be infinity large
-        % Make sure it's either 179 or 181 by random choice
-        %if(theta == 180)
-        %  theta = 180 + sign(rand(1));
-        %end
-
-        % Sometimes r kan be larger than R
-  %     if(r <= r_max)
-  %        P(theta, r) = P(theta, r) + 1;
-  %      end
-  %    end
-  %  end
-  %end
 
   % p is precent variable that describes the threshold for a line definition - Small lines avoid
   longest_top = max(P(:));
   P(P < longest_top*p) = 0;
 end
 
-function [K, M, R, T] = compute_lines(P, N, radius)
-  % Update size for P
-  [m, n] = size(P);
-
-  % Sort P as it was a vector
-  [~, index] = sort(P(:), 'descend');
-
+function [K, M, R, T] = hough_lines(A, N, index)
   % Create K, M, R and T - They are holders for the output
   K = zeros(1, N);
   M = zeros(1, N);
   R = zeros(1, N);
   T = zeros(1, N);
 
-  % Fill K and M
-  count = 1;
-  for k = 1:m*n
-    % Find the theta and r for the N largest value
-    [theta, r] = ind2sub([m, n], index(k));
+  % Fill
+  for i = 1:N
+    % Get the columns for class ID number i
+    B = A(index == i, :);
 
-    % Take minus 90 because we did + 90 above
-    theta = theta - 90;
+    % Find the maximum value at third column, which is the z-axis column
+    [~, max_index] = max(B(:, 3));
 
-    % Check if it's not the same theta and r again
-    if(k > 1)
-      if(compute_violate(theta, r, T, R, radius))
-        continue
-      end
-    end
+    % Get the theta and r, which is the x-axis and y-axis column
+    b = B(max_index, 1:2);
+    angles = b(1);
+    r = b(2);
 
-    % y = k*x + m can be expressed as x*sin(theta) + y*cos(theta) = r
-    v = deg2rad(theta);
-    K(count) = sin(v)/-cos(v);
-    M(count) = - r/-cos(v);
-    R(count) = r;
-    T(count) = theta;
-
-    % Count
-    count = count + 1;
-    if(count > N)
-      break;
-    end
+    % y = k*x + m can be expressed as x*sin(angles) + y*cos(angles) = r
+    v = deg2rad(angles);
+    K(i) = sin(v)/-cos(v);
+    M(i) = - r/-cos(v);
+    R(i) = r;
+    T(i) = angles;
   end
-end
-
-function violate = compute_violate(theta, r, T, R, radius)
-  % Check if they violate
-  theta_violate = abs(theta - T) < radius;
-  r_violate = abs(r - R) < radius;
-
-  % Check if they both violate
-  violate = and(length(find(r_violate > 0)), length(find(theta_violate > 0)));
-
-  %  violate = false;
-  %  for j = 1:count
-  %    % Find the difference
-  %    x = abs(theta - T(j));
-  %    y = abs(r - R(j));
-
-       % Check the difference is lower than radius
-  %    if(and(x < radius, y < radius))
-  %      violate = true;
-  %      break;
-  %    end
-  %  end
 end
