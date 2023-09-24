1cd49d4acdace3249871568b3f29f3d501adef27
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-09-24 19:03:51 +0200

    Update imgaussfilt.m

--- matave/+mi/imgaussfilt.m
+++ matave/+mi/imgaussfilt.m
@@ -1,5 +1,5 @@
 % Gaussian 2D filter
-% Input: data matrix(X)
+% Input: Data matrix(X)
 % Output: Y(Filtered data matrix)
 % Example 1: [Y] = mi.imgaussfilt(X);
 % Example 2: [Y] = mi.imgaussfilt(X, sigma);
