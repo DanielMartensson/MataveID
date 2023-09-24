5e01aaf9b71302034e510139cde71e7c989b7bef
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-09-24 23:19:34 +0200

    Update imgaussfilt.m - Improvements

--- matave/+mi/imgaussfilt.m
+++ matave/+mi/imgaussfilt.m
@@ -1,5 +1,5 @@
 % Gaussian 2D filter
-% Input: X(Data matrix), sigma(Standard deviation)
+% Input: data matrix(X)
 % Output: Y(Filtered data matrix)
 % Example 1: [Y] = mi.imgaussfilt(X);
 % Example 2: [Y] = mi.imgaussfilt(X, sigma);
@@ -34,24 +34,33 @@ function [Y] = imgaussfilt(varargin)
   kernel_size = round(6 * sigma);
 
   % Create mesh grid
-  [x, y] = meshgrid(1:kernel_size, 1:kernel_size);
+  [x, y] = meshgrid(-kernel_size:kernel_size, -kernel_size:kernel_size);
 
   % Create gaussian 2D kernel
   K_g = 1/(2*pi*sigma^2)*exp(-(x.^2 + y.^2)/(2*sigma^2));
 
-  % Normalization
-  K_g = K_g / sum(K_g(:));
-
   % Do conv2 with FFT
-  Y = conv2_fft(X, K_g);
+  Y = conv2_fft(X, K_g, kernel_size);
+
+  % Give the same brightness
+  Y = Y*sum(X(:))/sum(Y(:));
 end
 
-function G = conv2_fft(X, K)
+function G = conv2_fft(X, K, kernel_size)
   % Create kernel
   [m, n] = size(X);
   kernel = zeros(m, n);
   [m, n] = size(K);
-  kernel(1:m, 1:n) = K;
+
+  % Compute the sizes
+  m_middle = ceil(m/2);
+  n_middle = ceil(n/2);
+
+  % Insert kernel
+  kernel(1:m_middle, 1:n_middle) = K(m_middle:end, n_middle:end);
+  kernel(end, 1:n_middle) = K(1, n_middle:end);
+  kernel(1:m_middle, end) = K(m_middle:end, 1);
+  kernel(end, end) = K(1,1);
 
   % Do FFT2 on X and kernel
   A = fft2(X);
