cbd69aeeb143e64511fed1c1db720d16c5ee92b1
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-10-20 21:30:27 +0200

    Update fastExample.m

--- examples/fastExample.m
+++ examples/fastExample.m
@@ -6,17 +6,15 @@ X = imread(fullfile('..', 'data', 'face.jpg'));
 
 % Make image greyscale
 if size(X, 3) > 1
-	X =  rgb2gray(X);
-else
-	X = double(X);
+  X =  rgb2gray(X);
 end
 
 % Compute fast
-threshold = 10;
-fast_method = 12;
+threshold = 50;
+fast_method = 9;
 coordintes = mi.fast(X, threshold, fast_method);
 
 % Show
-imshow(X);
+imshow(uint8(X));
 hold on
 plot(coordintes(:,1), coordintes(:,2), 'r.')
