a300c9e3507cb9806b9b5ff59770ca02eee9a0bb
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-10-29 18:27:28 +0100

    Update and rename briskExample.m to orpExample.m

--- examples/orpExample.m
+++ examples/orpExample.m
@@ -8,16 +8,16 @@ X = imread(fullfile('..', 'data', 'hus.jpg'));
 
 % Make image greyscale
 if size(X, 3) > 1
-	X =  rgb2gray(X);
+  X =  rgb2gray(X);
 end
 
-% Compute BRISK
+% Compute Oriented FAST Rotated Pattern
 sigma1 = 1;             % Backgroun filtering
 sigma2 = 6;             % Filtering for the descriptors
 threshold_sobel = 127;  % Threshold for the corners
 threshold_fast = 50;    % Threshold for the corners
 fast_method = 9;        % FAST method: 9, 10, 11, 12
-[data, X1, X2, G, corners] = mi.brisk(X, sigma1, sigma2, threshold_sobel, threshold_fast, fast_method);
+[data, X1, X2, G, corners] = mi.orp(X, sigma1, sigma2, threshold_sobel, threshold_fast, fast_method);
 
 % Plot
 figure
