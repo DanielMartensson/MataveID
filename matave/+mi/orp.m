f856d535918c7dfd419d96eeb9ca6c2750824686
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-10-29 18:26:18 +0100

    Update orp.m

--- matave/+mi/orp.m
+++ matave/+mi/orp.m
@@ -1,7 +1,7 @@
 % Oriented FAST Rotated Pattern
 % Input: X(image), sigma1(background filtering), sigma2(descriptor filtering), threshold_sobel(corner filtering), threshold_fast(corner threshold), fast_method(enter: 9, 10, 11, 12)
 % Output: data(classification data), X1(filtered background), X2(filtered data for descriptors), G(gradients for the corners), corners, scores(corner scores)
-% Example 1: [data, X1, X2, G, corners, scores] = mi.brisk(X, sigma1, sigma2, threshold_sobel, threshold_fast, fast_method);
+% Example 1: [data, X1, X2, G, corners, scores] = mi.orp(X, sigma1, sigma2, threshold_sobel, threshold_fast, fast_method);
 % Author: Daniel Mårtensson, Oktober 27, 2023
 
 function [data, X1, X2, G, corners, scores] = orp(varargin)
